1d91652f3a0d60b5cba20727ce751a8442ee7a2a
sugumaran <sugumaran.ram@aspiresys.com>
2025-12-30 23:57:16 +0530

    update test file

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Daily Task Planner heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headingElement = screen.getByText(/Daily Task Planner/i);
+  expect(headingElement).toBeInTheDocument();
 });
